79fa73a5fc2b02e5898252085c5b043cf139e549
skydoves <skydoves@naver.com>
2021-04-19 19:08:28 +0900

    Implement LazyBonesCoroutines for supporting coroutines related jobs

---
+++ lazybones/src/main/java/com/skydoves/lazybones/LazyBonesCoroutines.kt
@@ -0,0 +1,73 @@
+/*
+ * Designed and developed by 2020 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skydoves.lazybones
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.addRepeatingJob
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+
+/** returns a [Job] lazily via [Lazybones] delegate for invoking the [block] on the onStarted [lifecycleScope]. */
+@JvmSynthetic
+@LazybonesWithNoInlines
+inline fun LifecycleOwner.launchOnStarted(
+  lazyThreadSafetyMode: LazyThreadSafetyMode = LazyThreadSafetyMode.NONE,
+  crossinline block: suspend CoroutineScope.() -> Unit
+): Lazybones<Job> {
+  val job = lifecycleScope.launchWhenStarted { block() }
+  return Lazybones(this, lazy(lazyThreadSafetyMode) { job })
+}
+
+/** returns a [Job] lazily via [Lazybones] delegate for invoking the [block] on the onCreated [lifecycleScope]. */
+@JvmSynthetic
+@LazybonesWithNoInlines
+inline fun LifecycleOwner.launchOnCreated(
+  lazyThreadSafetyMode: LazyThreadSafetyMode = LazyThreadSafetyMode.NONE,
+  crossinline block: suspend CoroutineScope.() -> Unit
+): Lazybones<Job> {
+  val job = lifecycleScope.launchWhenCreated { block() }
+  return Lazybones(this, lazy(lazyThreadSafetyMode) { job })
+}
+
+/** returns a [Job] lazily via [Lazybones] delegate for invoking the [block] on the onResume [lifecycleScope]. */
+@JvmSynthetic
+@LazybonesWithNoInlines
+inline fun LifecycleOwner.launchOnResume(
+  lazyThreadSafetyMode: LazyThreadSafetyMode = LazyThreadSafetyMode.NONE,
+  crossinline block: suspend CoroutineScope.() -> Unit
+): Lazybones<Job> {
+  val job = lifecycleScope.launchWhenResumed { block() }
+  return Lazybones(this, lazy(lazyThreadSafetyMode) { job })
+}
+
+/**
+ * Collects from the flow when the lifecycle is at least [Lifecycle.State.STARTED] and
+ *  STOPS the collection when it's STOPPED.
+ * It automatically restarts collecting when the lifecycle is [Lifecycle.State.STARTED] again.
+ */
+@JvmSynthetic
+@LazybonesWithNoInlines
+inline fun LifecycleOwner.addOnRepeatingJob(
+  state: Lifecycle.State,
+  lazyThreadSafetyMode: LazyThreadSafetyMode = LazyThreadSafetyMode.NONE,
+  crossinline block: suspend CoroutineScope.() -> Unit
+): Lazybones<Job> {
+  val job = addRepeatingJob(state = state) { block() }
+  return Lazybones(this, lazy(lazyThreadSafetyMode) { job })
+}
